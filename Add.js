7b076055811ec28ed67bc7fea9ec2962767ecdf0
Khaair <khair.cse.iu@gmail.com>
2021-10-28 00:46:50 +0600

    added

--- Add.js
+++ Add.js
@@ -1,5 +1,7 @@
-var x = 5;
-var y = 6;
-var z = x + y;
+let x, y, z;  // Statement 1
+x = 5;        // Statement 2
+y = 6;        // Statement 3
+z = x + y;    // Statement 4
+
 document.getElementById("demo").innerHTML =
-"The value of z is: " + z;
+"The value of z is " + z + "."
