082cdae2ab4416e879755c0cff07b116073398b7
Khaair <khair.cse.iu@gmail.com>
2021-12-14 18:59:05 +0600

    Update

---
+++ arraymethod.js
@@ -0,0 +1,22 @@
+// var arr = [2, 4, 5, 8]
+
+// for(var i = 0;i<=arr.length;i++){
+
+//     console.log(arr[i])
+// }
+
+// var a = ["a", "b", "c"];
+// let ro=a.forEach(c);
+
+
+
+// function c(r,t,k){
+//     return r+8;
+// }
+
+// console.log(ro)
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+var res = fruits.toString()
+console.log(res)
---
+++ paperwork.js
@@ -0,0 +1,10 @@
+function paper(n,m){
+    var res = (n >= 0 && m >= 0 ? n * m : 0)
+
+    return res
+        
+}
+
+
+
+console.log(paper(10,10))
