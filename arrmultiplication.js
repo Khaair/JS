f3d9249e9e8dc460166c84b5dece20adfb5cd4fc
Khaair <khair.cse.iu@gmail.com>
2021-12-14 20:09:06 +0600

    Update

---
+++ arrmultiplication.js
@@ -0,0 +1,26 @@
+// var arr = [2, 4, 6]
+
+// var sum = 1
+
+// for(var i = 0; i <= arr.length-1;i++){
+//     var sum = sum * arr[i]
+   
+// }
+
+// console.log(sum)
+var arr = []
+
+function product(){
+
+    var sum = 1
+ for( var i = 0; i <= arr.length-1; i++){
+    
+     sum = sum * arr[i]
+ }
+ return sum;
+}
+
+
+
+console.log( product(arr[2, 4, 6]))
+
