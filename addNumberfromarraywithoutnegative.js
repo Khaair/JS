423beacae3bc9084976c499ced07fffe9b1f1f99
Khaair <khair.cse.iu@gmail.com>
2021-12-15 21:59:09 +0600

    Update

--- addNumberfromarraywithoutnegative.js
+++ addNumberfromarraywithoutnegative.js
@@ -1,53 +1,27 @@
-Skip welcome & menu and move to editor
- 
-Textarea editor mode
-JS Bin features
-Getting started
-Keyboard Shortcuts
-Exporting/importing gist
- 
-Pro features
-Private bins
-Vanity URLs
-Upgrade to pro now
- 
-Blog
-The Return and The Refactor
-Help
-Experimental Features
-How to clear the console
- 
-Donate to JS Bin ❤
-Support JS Bin to keep the project open source & MIT for all
-Follow @js_bin on twitter
-By using JS Bin you agree to our legal terms
 // var arr = [2, 3, 5, 6,-2]
-​
-​
-​
+
+
+
 // console.log(add)
-​
+
 function addd(arr){
   
   
-  var filterr = arr.filter( x => x   > 0)
-​
-var add = filterr.reduce((a,b) => a+b)
-​
-return add
-}
-​
-console.log(addd([2, 3, 5, 6,-2]))
-​
-​
-// function positiveSum(arr) {
-//   return arr.filter(x => x > 0).reduce((a, b) => a+b, 0);
-// }
-​
-// console.log(positiveSum([2, 3, 5, 6]))
-​
-​
-​
-16
-Bin info
-just now
+    var filterr = arr.filter( x => x   > 0)
+  
+  var add = filterr.reduce((a,b) => a+b)
+  
+  return add
+  }
+  
+  console.log(addd([2, 3, 5, 6,-2]))
+  
+  
+  // function positiveSum(arr) {
+  //   return arr.filter(x => x > 0).reduce((a, b) => a+b, 0);
+  // }
+  
+  // console.log(positiveSum([2, 3, 5, 6]))
+  
+  
+  
