76b8133e3b2dbd9935db263e174cd3cf1b1a3b50
Khaair <khair.cse.iu@gmail.com>
2021-12-15 20:15:44 +0600

    Update

---
+++ reverseString.js
@@ -0,0 +1,10 @@
+var str = "Hello"
+
+var newarray = str.split("")
+var reversee = newarray.reverse()
+
+var joinn = reversee.join('')
+
+console.log(newarray)
+console.log(reversee)
+console.log(joinn)
