2b113ba5df86a6c8b02f45faebe409a9764cb556
Khaair <khair.cse.iu@gmail.com>
2021-12-15 07:58:44 +0600

    Update

---
+++ map.js
@@ -0,0 +1,10 @@
+var arr = [1, 2, 3, 4]
+
+var newarr = arr.map(function(value, index, arr){
+  var mul = value * value
+  return mul
+  
+//   console.log(mul)
+})
+console.log(arr)
+console.log(newarr)
