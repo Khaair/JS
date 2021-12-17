4c35b2c9029f1f9ab98309b928e599af314bdd46
Khaair <khair.cse.iu@gmail.com>
2021-12-17 09:31:01 +0600

    Update

---
+++ maxMinFromString.js
@@ -0,0 +1,18 @@
+var str = "9874"
+
+var arr = str.split("")
+
+const maxx = Math.max(...arr);
+
+const minn = Math.min(...arr);
+
+
+// var inttt = parseInt(mxxx)
+console.log(maxx + " " + minn)
+
+// var arr = [3, 4, 7, 8]
+
+
+// const max = Math.max(...arr);
+
+// console.log(max)
--- stringYesNo.js
+++ stringYesNo.js
@@ -8,3 +8,21 @@ function boolToWord(bool){
   }
   }
   console.log(boolToWord(false))
+
+  // function booleanToString(b){
+//   return b.toString()
+  
+  
+// }
+
+// console.log(booleanToString(true))
+
+// function boolToWord( bool ){
+//   if(bool = "true"){
+//     return "Yes"
+//   }
+//   else if(bool = "False"){ 
+//     return "No"}
+// }
+
+// console.log(boolToWord("False"))
