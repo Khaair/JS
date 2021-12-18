88796bf4bf53b8b2106bc0bc3ac2c0583535e608
Khaair <khair.cse.iu@gmail.com>
2021-12-18 20:37:31 +0600

    Update

--- sumoftwosmallestarrayelement.js
+++ sumoftwosmallestarrayelement.js
@@ -1,10 +1,6 @@
-function sumTwoSmallestNumbers(numbers) {  
-    var sortt = numbers.sort(function(a, b){return a - b});
+function addBinary(a,b) {
+    var sum = a+b
+    var tt = (sum >>> 0).toString(2)
+    return tt
   
-  var ff = [sortt[0] ,sortt[1]]
-  var sum = 0;
-  for(i=0;i<=ff.length-1;i++){
-    sum+=ff[i]
-  }
-  return sum
   }
