c1712ec23a2a12c932b45148e3615288a5380b78
Khair <khair.cse.iu@gmail.com>
2022-04-07 17:16:58 +0600

    update

--- squaresumpractice.js
+++ squaresumpractice.js
@@ -45,10 +45,10 @@ console.log(h)
 
 
 
-// const array1 = [1, 4, 9, 16];
+const array1 = [1, 4, 9, 16];
 
-// // pass a function to map
-// const map1 = array1.map(x => x * 2);
+// pass a function to map
+const map1 = array1.map(x => x * 2);
 
-// console.log(map1);
-// // expected output: Array [2, 8, 18, 32]
+console.log(map1);
+// expected output: Array [2, 8, 18, 32]
