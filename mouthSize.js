05f2943e102a6eb692b55f8ad31cffc479a9a73b
khaair <khair.cse.iu@gmail.com>
2022-05-24 16:27:03 +0600

    update

---
+++ mouthSize.js
@@ -0,0 +1,12 @@
+function mouthSize(animal) {
+  
+    let a = animal.toLowerCase()
+  if(a=='alligator'){
+    return 'small'
+  }
+    else{
+      return 'wide'
+    }
+  }
+  
+  console.log(mouthSize("ALLIGATOR"))
